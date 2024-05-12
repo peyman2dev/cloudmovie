a26b9134cc0006ab08b9037275cdf95d00e698c7
peyman2dev <peyman2dev@gmail.com>
2024-05-12 11:58:41 +0330

    Pagination feature added

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,11 +1,15 @@
 import { movies } from "../modules/api/api.js";
+import pagination from "../modules/pagination.js";
 const $ = document
 
-const moviesContainer = $.querySelector(".movies-container")
+const moviesContainer = $.querySelector(".movies")
+const buttonsWrapper = $.querySelector(".pagination-buttons")
 
-console.log(movies)
+pagination({
+    container: moviesContainer,
+    items: movies,
+    rows: 5,
+    buttonsWrapper: buttonsWrapper
+})
 
 
-// movies.forEach(movie => {
-//     moviesContainer.innerHTML = 's'
-// })
